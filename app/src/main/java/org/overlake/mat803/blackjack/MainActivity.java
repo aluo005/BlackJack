cac97c8e0399d17aa8c2d5cb4b5ed4d2d98adff6
Albert Luo <albertluo040605@gmail.com>
2022-05-25 20:15:12 -0700

    changed names of drawables

--- app/src/main/java/org/overlake/mat803/blackjack/MainActivity.java
+++ app/src/main/java/org/overlake/mat803/blackjack/MainActivity.java
@@ -2,14 +2,40 @@ package org.overlake.mat803.blackjack;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.Image;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+
+import org.overlake.mat803.blackjack.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding mBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        mBinding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(mBinding.getRoot());
+        mBinding.cardDeck.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ImageView imageView = new ImageView(MainActivity.this);
+                imageView.setImageResource(R.drawable.ace_of_clubs);
+
+                addView(imageView, 200, 200);
+            }
+        });
+    }
+
+    public void addView(ImageView imageView, int width, int height){
+        LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(width, height);
+        layoutParams.setMargins(10,10,10,10);
+
+        imageView.setLayoutParams(layoutParams);
+        mBinding.layout.addView(imageView);
     }
 
 }
